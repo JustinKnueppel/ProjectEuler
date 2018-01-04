7691154070eaacbad32c412e4aa7bd5b98f2b9f8
Justin Knueppel <justinknueppel@gmail.com>
2018-01-03 20:55:18 -0500

    solved problem 7

---
+++ problem7.py
@@ -0,0 +1,29 @@
+def countup(num):
+    #counts up from 1-infinity
+    i = num
+    while True:
+        i += 1
+        yield i
+
+def getprime(primes, num):
+    curnum = -1
+    for i in countup(num):
+        add = True
+        for prime in primes:
+            if i % prime == 0:
+                add = False
+                break
+        if add:
+            curnum = i
+            break
+    return curnum
+
+counter = 1
+curnum = 1
+primes = []#2, 3, 5, 7]
+while counter <= 10001:
+    curnum = getprime(primes, curnum)
+    print(curnum)
+    primes.append(curnum)
+    counter += 1
+print(primes[-1])
