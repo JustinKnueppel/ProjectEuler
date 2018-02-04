52cde768cadd04386d97f9a19f9751e37b613106
Justin Knueppel <justinknueppel@gmail.com>
2018-02-03 21:13:19 -0500

    add comments to problem 20

--- problem20.py
+++ problem20.py
@@ -1,6 +1,12 @@
 """Solution to problem 20 on project euler"""
+# https://projecteuler.net/problem=20
 
-#find the total of the digits of 100!
+# n! means n × (n − 1) × ... × 3 × 2 × 1
+
+# For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+# and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+# Find the sum of the digits in the number 100!
 
 def factorial(num):
     """returns the factorial of the argument (num >= 0)"""
